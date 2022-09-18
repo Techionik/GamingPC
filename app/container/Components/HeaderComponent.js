8da84e1c7f433805d73db0fc99b53aab5e33bc6c
AhmadChaudhry <ahmad.chaudhry070@gmail.com>
2022-09-19 02:42:46 +0500

    Font Family

--- app/container/Components/HeaderComponent.js
+++ app/container/Components/HeaderComponent.js
@@ -2,17 +2,19 @@ import {Image, Text, TouchableOpacity, View} from "react-native";
 import {Color, Constants} from "../../common";
 import React from "react";
 import {useNavigation} from "@react-navigation/native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 
-export default function HeaderComponent({Props, title,Drawer}) {
+export default function HeaderComponent({Props, title,Drawer,Location}) {
     const navigation=useNavigation()
     const {t, language} = Props
     return (
         <View style={{flexDirection: "row",marginHorizontal:10, alignItems: "center", marginTop: 10,}}>
-            <TouchableOpacity onPress={()=>{navigation.navigate("DrawerScreen")}} style={{borderRadius: 10, backgroundColor: Color.grayback, padding: 20}}>
+            <TouchableOpacity onPress={()=>{navigation.navigate("DrawerScreen")}} style={{borderRadius: 5, backgroundColor: Color.grayback, padding: 10}}>
+                <MaterialIcons name={"sort"} size={25} color={Color.grayIn}/>
             </TouchableOpacity>
             {Drawer == true ?
-                <View style={{flex:1,alignItems:"center",}}>
+                <View style={{flex:Location===false?0.9:1,alignItems:"center",}}>
                     <Text style={{fontSize: 16, fontFamily: Constants.fontFamilyRegular, color: "#000"}}>{title}</Text></View> :
                <>
                 <View style={{marginLeft: 10, alignSelf: "flex-start"}}>
@@ -25,7 +27,10 @@ export default function HeaderComponent({Props, title,Drawer}) {
                 </View>
                 <View style={{flex:1}}/>
                 <Image source={require('../../images/NotficationICon.png')} resizeMode={"contain"} style={{height:undefined,width:"10%",marginRight:10,aspectRatio:1}}/></>}
-            <TouchableOpacity style={{
+            {Location===false?
+               null
+                :
+                <TouchableOpacity style={{
                 width: 80,
                 paddingVertical: 10,
                 borderRadius: 7,
@@ -36,15 +41,16 @@ export default function HeaderComponent({Props, title,Drawer}) {
                 flexDirection: "row"
             }}>
                 <Image source={require('../../images/MarkerPin.png')} resizeMode={"contain"}
-                       style={{height: undefined, width: "20%", aspectRatio: 1, tintColor: Color.grayIn}}/>
+                style={{height: undefined, width: "20%", aspectRatio: 1, tintColor: Color.grayIn}}/>
                 <Text numberOfLines={1} style={{
-                    fontSize: 12,
-                    fontFamily: Constants.fontFamilyRegular,
-                    color: "#000",
-                    marginLeft: 5,
-                    width: "70%"
-                }}>St-14, Mall City</Text>
-            </TouchableOpacity>
+                fontSize: 12,
+                fontFamily: Constants.fontFamilyRegular,
+                color: "#000",
+                marginLeft: 5,
+                width: "70%"
+            }}>St-14, Mall City</Text>
+                </TouchableOpacity>
+            }
         </View>
     )
 }
--- app/container/HomeScreen/MyBookingScreen.js
+++ app/container/HomeScreen/MyBookingScreen.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native'
 import withLanguage from "../../config/withLanguage";
 import {Color, Constants} from "../../common";
+import HeaderComponent from "../Components/HeaderComponent";
 
 
 
@@ -21,7 +22,10 @@ class MyBookingScreens extends React.Component {
     render() {
         const {t, language} = this.props.value
         return (
-            <View style={{flex: 1,backgroundColor:Color.primary}}>
+            <View style={{flex: 1,backgroundColor:"#ffff"}}>
+                <HeaderComponent Location={false} title={"My Bookings"} Drawer={true} Props={this.props.value} />
+
+
 
             </View>
 
