e41660f15656323d77cce41c2a555215116ec68c
AhmadChaudhry <ahmad.chaudhry070@gmail.com>
2022-09-19 03:38:40 +0500

    My booking Screen

---
+++ app/container/Components/MyBookingsComponent.js
@@ -0,0 +1,28 @@
+import {Image, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {Color, Constants} from "../../common";
+import React from "react";
+
+
+export default function MyBookingsComponent({Style,image,title,titleStyle,onPress,}) {
+    return (
+        <View  style={[{
+            borderRadius: 10,
+            backgroundColor: "#fff",
+            paddingVertical: 10,
+            justifyContent:"center",
+            alignItems:"center",
+            marginVertical: 10,
+            borderColor:Color.primary,
+            borderWidth:0.4,
+        }, Style]}>
+            <View style={{flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}>
+
+           <Image resizeMode={"center"} source={image} style={{height:50,width:60}}/>
+            <Text style={[{fontSize:16,fontFamily:Constants.fontFamilyRegular,color:"#000"},titleStyle]}>{title}</Text>
+
+            <Text style={[{fontSize:12,fontFamily:Constantss},titleStyle]}>{title}</Text>
+        </View>
+
+        </View>
+    )
+}
--- app/container/HomeScreen/MyBookingScreen.js
+++ app/container/HomeScreen/MyBookingScreen.js
@@ -9,6 +9,7 @@ import {
 import withLanguage from "../../config/withLanguage";
 import {Color, Constants} from "../../common";
 import HeaderComponent from "../Components/HeaderComponent";
+import MyBookingsComponent from "../Components/MyBookingsComponent";
 
 
 
@@ -16,6 +17,62 @@ class MyBookingScreens extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            bookingList:[
+                {
+                    image:require("../../images/slonImage.png"),
+                    title:"The big Tease Saloon",
+                    status:"Upcoming",
+                    date:"Monday 19th August,2022",
+                    startTime:"11:30 am",
+                    endTime:"12:00 pm",
+                    diffTime:"60 min ",
+                    taskList:[
+                        {
+                            image:require("../../images/nailPolishImage.png"),
+                            title:"Nail Polish Art",
+                            rate:"34",
+                        },
+                        {
+                            image:require("../../images/spaImage.png"),
+                            title:"Special Spa Day",
+                            rate:"224",
+                        }
+                    ]
+                },
+                {
+                    image:require("../../images/slonImage.png"),
+                    title:"The big Tease Saloon",
+                    status:"Upcoming",
+                    date:"Monday 19th August,2022",
+                    startTime:"11:30 am",
+                    endTime:"12:00 pm",
+                    diffTime:"60 min ",
+                    taskList:[
+                        {
+                            image:require("../../images/spaImage.png"),
+                            title:"Special Spa Day",
+                            rate:"224",
+                        }
+                    ]
+                },
+                {
+                    image:require("../../images/slonImage.png"),
+                    title:"The big Tease Saloon",
+                    status:"Upcoming",
+                    date:"Monday 19th August,2022",
+                    startTime:"11:30 am",
+                    endTime:"12:00 pm",
+                    diffTime:"60 min ",
+                    taskList:[
+                        {
+                            image:require("../../images/nailPolishImage.png"),
+                            title:"Nail Polish Art",
+                            rate:"34",
+                        },
+
+                    ]
+                }
+            ]
         }
     }
 
@@ -25,6 +82,11 @@ class MyBookingScreens extends React.Component {
             <View style={{flex: 1,backgroundColor:"#ffff"}}>
                 <HeaderComponent Location={false} title={"My Bookings"} Drawer={true} Props={this.props.value} />
 
+
+                <FlatList data={this.state.bookingList}  renderItem={({item, index}) =>
+                    <MyBookingsComponent title={item.title} image={item.image}
+                     />
+                }/>
 
 
             </View>
