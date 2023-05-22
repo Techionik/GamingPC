4b05dd027e7f25414eff096c6448f1cf80542a37
Mudassir Masood <mudassirmasood0805@gmail.com>
2023-05-22 16:57:20 +0500

    initial commit for Enhancer

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -7,49 +7,7 @@ import {compose} from "redux";
 import {connect} from "react-redux";
 import {languageSelector, themeSelector} from "../redux/app/selectors";
 import SplashContainer from "../container/SplashContainer";
-import LoginScreen from "../container/AuthenticationScreens/LoginScreen";
 import MapScreen from "../container/MapScreen";
-
-import HomeScreen from "../container/HomeScreen/HomeScreen";
-import EarningScreen from "../container/HomeScreen/EarningScreen";
-import JobsScreen from "../container/HomeScreen/JobsScreen";
-import ForgotPasswordScreen from "../container/AuthenticationScreens/ForgotPasswordScreen";
-import VerificationScreen from "../container/AuthenticationScreens/VerifiactionScreen";
-import RateAndReviewScreen from "../container/HomeScreen/RateAndReviewScreen";
-import SettingScreen from "../container/HomeScreen/SettingScreen";
-import SendComplain from "../container/HomeScreen/SendComplain";
-import TermConditionScreen from "../container/HomeScreen/TermConditionScreen";
-import ProfileScreen from "../container/HomeScreen/ProfileScreen";
-import AgrementScreen from "../container/HomeScreen/AgrementScreen";
-
-
-import AttendenceScreen from "../container/HomeScreen/AttendanceScreen";
-import LeaveScreen from "../container/HomeScreen/LeaveScreen";
-import BreakTimeScreen from "../container/HomeScreen/BreakTimeScreen";
-import LeavesScreen from "../container/HomeScreen/AdminScreens/LeavesScreen";
-import PendingScreen from "../container/HomeScreen/AdminScreens/PendingScreen";
-import PayRoleScreen from "../container/HomeScreen/AdminScreens/PayRoleScreen";
-import ViewCurrentLeave from "../container/HomeScreen/AdminScreens/ViewCurrentLeave";
-import AddNewPassword from "../container/AuthenticationScreens/AddNewPassword";
-import SendLeaveScreen from "../container/HomeScreen/SendLeaveScreen";
-import UpdatePasswordScreen from "../container/HomeScreen/UpdatePasswordScreen";
-import ComplainsScreen from "../container/HomeScreen/AdminScreens/ComplainsScreen";
-import ViewCurrentComplain from "../container/HomeScreen/AdminScreens/ViewCurrentComplain";
-import EmployComplainsScreen from "../container/HomeScreen/EmployComplainsScreen";
-import AllUserScreen from "../container/HomeScreen/AdminScreens/AllUserScreen";
-import ViewCurrentUser from "../container/HomeScreen/AdminScreens/ViewCurrentUser";
-import EditProfileScreen from "../container/HomeScreen/AdminScreens/EditProfileScreen";
-import PendingLeaves from "../container/HomeScreen/AdminScreens/PendingLeaves";
-import AcceptedLeaves from "../container/HomeScreen/AdminScreens/AcceptedLeaves";
-import RejectedLeaves from "../container/HomeScreen/AdminScreens/RejectedLeaves";
-import AboutUsScreen from "../container/HomeScreen/AboutUsScreen";
-import PendingComplains from "../container/HomeScreen/AdminScreens/PendingComplains";
-import AcceptedComplains from "../container/HomeScreen/AdminScreens/AcceptedComplains";
-import RejectedComplains from "../container/HomeScreen/AdminScreens/RejectedComplains";
-
-
-
-
 
 
 const Stack = createStackNavigator()
@@ -58,43 +16,7 @@ const AppStack = ({}) => {
     return (
         <Stack.Navigator initialRouteName={"SplashContainer"}>
             <Stack.Screen name={"SplashContainer"} component={SplashContainer} options={{headerShown: false}}/>
-            <Stack.Screen name={"LoginScreen"} component={LoginScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"MapScreen"} component={MapScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"ForgotPasswordScreen"} component={ForgotPasswordScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"VerificationScreen"} component={VerificationScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"EarningScreen"} component={EarningScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"AgrementScreen"} component={AgrementScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"JobsScreen"} component={JobsScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"HomeScreen"} component={HomeScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"RateAndReviewScreen"} component={RateAndReviewScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"SettingScreen"} component={SettingScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"SendComplain"} component={SendComplain} options={{headerShown: false}}/>
-            <Stack.Screen name={"TermConditionScreen"} component={TermConditionScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"ProfileScreen"} component={ProfileScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"AttendenceScreen"} component={AttendenceScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"LeaveScreen"} component={LeaveScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"BreakTimeScreen"} component={BreakTimeScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"LeavesScreen"} component={LeavesScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"PendingScreen"} component={PendingScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"PayRoleScreen"} component={PayRoleScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"ViewCurrentLeave"} component={ViewCurrentLeave} options={{headerShown: false}}/>
-            <Stack.Screen name={"AddNewPassword"} component={AddNewPassword} options={{headerShown: false}}/>
-            <Stack.Screen name={"SendLeaveScreen"} component={SendLeaveScreen} options={{headerShown: false}}/>
 
-            <Stack.Screen name={"UpdatePasswordScreen"} component={UpdatePasswordScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"ComplainsScreen"} component={ComplainsScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"ViewCurrentComplain"} component={ViewCurrentComplain} options={{headerShown: false}}/>
-            <Stack.Screen name={"EmployComplainsScreen"} component={EmployComplainsScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"AllUserScreen"} component={AllUserScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"ViewCurrentUser"} component={ViewCurrentUser} options={{headerShown: false}}/>
-            <Stack.Screen name={"EditProfileScreen"} component={EditProfileScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"PendingLeaves"} component={PendingLeaves} options={{headerShown: false}}/>
-            <Stack.Screen name={"AcceptedLeaves"} component={AcceptedLeaves} options={{headerShown: false}}/>
-            <Stack.Screen name={"RejectedLeaves"} component={RejectedLeaves} options={{headerShown: false}}/>
-            <Stack.Screen name={"AboutUsScreen"} component={AboutUsScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={"PendingComplains"} component={PendingComplains} options={{headerShown: false}}/>
-            <Stack.Screen name={"AcceptedComplains"} component={AcceptedComplains} options={{headerShown: false}}/>
-            <Stack.Screen name={"RejectedComplains"} component={RejectedComplains} options={{headerShown: false}}/>
         </Stack.Navigator>
     )
 }
@@ -107,15 +29,12 @@ class AppNavigator extends React.Component {
             navigationDone: false
         }
     }
-
     componentDidMount() {
         setTimeout(() => {
             this.setState({done: false})
         }, 1500)
 
     }
-
-
     render() {
 
         return (
