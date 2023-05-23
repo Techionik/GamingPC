a08137eb77ecfe0657d9eaf57a24709e7ca2c69f
Mudassir Masood <mudassirmasood0805@gmail.com>
2023-05-23 16:29:42 +0500

    Splash Screen

--- app/container/AuthScreens/SocialSignupScreen.js
+++ app/container/AuthScreens/SocialSignupScreen.js
@@ -1,11 +1,23 @@
 import React from "react";
-import {View} from "react-native";
+import {ImageBackground, Text, View} from "react-native";
+import {SocialButtonComponent} from "../Components/SocialButtonComponent";
+import {Constants} from "../../common";
 
 
 export const SocialSignupScreen=()=>{
     return(
-        <View>
+        <ImageBackground source={require('../../images/SocialBackground.png')} style={{flex:1,justifyContent:"flex-end",height:undefined,width:"100%",aspectRatio:0.479,alignSelf:"center"}}>
+            <View style={{flex:0.4,backgroundColor:"rgba(255,255,255,0.42)",justifyContent:"flex-end",borderTopLeftRadius:25,borderTopRightRadius:25}}>
+            <View style={{backgroundColor:"#fff",flex:0.9,borderTopLeftRadius:25,borderTopRightRadius:25,justifyContent:"center"}}>
+                <View style={{marginHorizontal:20}}>
+                <SocialButtonComponent title={"Continue with Google"} image={require('../../images/Google.png')}/>
+                <SocialButtonComponent title={"Continue with Facebook"} image={require('../../images/FacebookCircled.png')}/>
+                <SocialButtonComponent title={"Continue with Instagram"} image={require('../../images/Instagram.png')}/>
+                </View>
 
-        </View>
+            </View>
+            <Text style={{color:"#000",backgroundColor:"#fff",textAlign:"center",fontFamily:Constants.fontFamilyRegular,fontSize:12}}>Powered by TECHIONIK</Text>
+            </View>
+        </ImageBackground>
     )
 }
---
+++ app/container/Components/SocialButtonComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Image, Text, TouchableOpacity} from "react-native";
+import {Color} from "../../common";
+
+
+export const SocialButtonComponent=({title,image,style})=>{
+    return(
+        <TouchableOpacity style={[{alignItems:"center",marginVertical:5,flexDirection:"row",backgroundColor:Color.primary,borderRadius:30,paddingHorizontal:20,paddingVertical:10},style]}>
+            <Image source={image} style={{height:undefined,width:"12%",marginRight:20,aspectRatio:1}}/>
+            <Text style={{fontSize:18,}}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
